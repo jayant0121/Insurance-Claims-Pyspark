6370e2c2c3545ad708d124b2240b67e5c74ffe59
Jayant Asudhani <jayant.a@sigmoidanalytics.com>
2026-01-18 16:43:50 +0530

    Separate raw data generation from main pipeline execution

--- main.py
+++ main.py
@@ -1,15 +1,11 @@
-from pyspark.sql import functions as F
+from src.read_data import read_claims_raw
 from src.spark_session import get_spark
-from src.data_generator import generate_random_data
 
 spark = get_spark()
+claims_df = read_claims_raw(
+    spark,
+    "data/raw/claims_raw"
+)
 
-claims_raw = generate_random_data(spark, num_records=1_000_000)
-claims_raw.printSchema()
-claims_raw.count()
-claims_raw.select("claim_id").distinct().count()
-claims_raw.groupBy("claim_id").count().orderBy(F.desc("count")).show(5)
-
-
-claims_raw.write.mode("overwrite").partitionBy("claim_date").parquet("data/raw/claims_raw")
-
+claims_df.printSchema()
+claims_df.show(5, truncate=False)
